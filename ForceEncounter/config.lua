c68e58aa37fffe9c747db80975bcaffb3a6282ae
RedContritio <RedContritio@qq.com>
2026-06-21 23:58:33 +0800

    Bump ForceEncounter to 0.0.0.23

--- ForceEncounter/config.lua
+++ ForceEncounter/config.lua
@@ -1,7 +1,7 @@
 ﻿return {
     Title = "情难自已",
     Description = "为玩家提供接口，主动触发角色之间的正式版强制行为。",
-    Version = "0.0.0.22",
+    Version = "0.0.0.23",
     Author = "RedContritio",
     Source = 0,
     Cover = "",
